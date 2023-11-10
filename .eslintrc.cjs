8ada013e61fe2af8457186dec86f61e6722273e4
Svetlana <svetlana.moiseienko.web@gmail.com>
2023-11-10 10:58:29 +0300

    feat: configure prettier and esLint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,12 +6,14 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    'prefer-const': true,
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
