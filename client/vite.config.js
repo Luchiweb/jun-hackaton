2a8931f44a65793787ecf60ba613f531b058ec96
mithra-one <mithra4web@gmail.com>
2023-11-10 21:13:40 +0300

    feat: add backend proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,6 +6,14 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_BASE_URL,
+          secure: false,
+        },
+      },
+    },
     envPrefix: "VITE_",
     plugins: [
       react(),
