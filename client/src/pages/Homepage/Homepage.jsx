868c85cfaef8b62fc24e032a287b53e7ca5a3fd9
mithra-one <mithra4web@gmail.com>
2023-11-11 17:58:04 +0300

    feat: add gameinfo

--- client/src/pages/Homepage/Homepage.jsx
+++ client/src/pages/Homepage/Homepage.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import s from './Homepage.module.scss';
 import Head from '../../components/Head/Head';
 import Field from '../../components/Field/Field';
+import GameInfo from '../../components/GameInfo/GameInfo';
 
 const Homepage = () => {
   return (
     <div className={s.homepage}>
       <Head />
       <Field />
+      <GameInfo />
     </div>
   );
 };
