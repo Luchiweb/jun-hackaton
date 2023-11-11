c3022599b0d516673100ca60e3fc47ccb94d238c
mithra-one <mithra4web@gmail.com>
2023-11-11 12:15:24 +0300

    feat: add homepage background

---
+++ client/src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Homepage.module.scss';
+
+const Homepage = () => {
+  return (
+    <>
+      <h1 className={s.green}>Hello World</h1>
+    </>
+  );
+};
+
+export default Homepage;
