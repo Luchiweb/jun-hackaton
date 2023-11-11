fb346e6943176b150c46e39924b9b5660831052a
mithra-one <mithra4web@gmail.com>
2023-11-11 13:25:35 +0300

    feat: google fonts preload

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import Unfonts from 'unplugin-fonts/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -16,6 +17,32 @@ export default defineConfig(({ mode }) => {
       },
     },
     envPrefix: 'VITE_',
-    plugins: [react(), svgr()],
+    plugins: [
+      react(),
+      svgr(),
+      Unfonts({
+        google: {
+          preconnect: true,
+          /**
+           * values: auto, block, swap(default), fallback, optional
+           * default: 'swap'
+           */
+          display: 'swap',
+          /**
+           * define where the font load tags should be inserted
+           * default: 'head-prepend'
+           *   values: 'head' | 'body' | 'head-prepend' | 'body-prepend'
+           */
+          injectTo: 'head-prepend',
+          families: [
+            {
+              name: 'Nunito',
+              styles: 'wght@200;400;500;700;800',
+              defer: true,
+            },
+          ],
+        },
+      }),
+    ],
   };
 });
