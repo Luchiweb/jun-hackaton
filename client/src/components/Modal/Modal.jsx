bb3df5d097b939d85f5d99021c553d83edf31073
Svetlana <svetlana.moiseienko.web@gmail.com>
2023-11-11 22:31:57 +0300

    feat: add name field

--- client/src/components/Modal/Modal.jsx
+++ client/src/components/Modal/Modal.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import s from './Modal.module.scss';
 import Button from '../Button/Button';
 
 const Modal = ({ closeModal, moves, timer, restartGame }) => {
+  const [name, setName] = useState('');
+  const [isSaved, setIsSaved] = useState(false);
+
+  const handleInput = (e) => {
+    setName(e.target.value);
+  };
+
+  const saveResult = () => {
+    alert(`Сохранен результат: ${name}`);
+    setName('');
+    setIsSaved(true);
+  };
+
   return (
     <div className={s.modal}>
       <div className={s.modalContent}>
@@ -14,6 +27,25 @@ const Modal = ({ closeModal, moves, timer, restartGame }) => {
           <p>
             Время: <span>{timer}s</span>
           </p>
+        </div>
+        <div>
+          <h2>Занеси свой успех в таблицу лидеров</h2>
+          <div className={s.form}>
+            <input
+              value={name}
+              onChange={(e) => handleInput(e)}
+              type="text"
+              placeholder="Иван Иванов"
+              className={s.textInput}
+            />
+            <Button
+              label="Сохранить"
+              height="35px"
+              width="30%"
+              onClick={saveResult}
+              disabled={isSaved}
+            />
+          </div>
         </div>
         <Button
           label="Играть снова!"
